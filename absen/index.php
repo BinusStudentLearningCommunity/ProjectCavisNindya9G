76383adb499baba7e62e6b78dc5be7dd0ef817e7
MegaChevElia <megachevalierblanc@gmail.com>
2016-06-01 12:31:47 +0700

    tambahan logo dan tulisan NIM

--- absen/index.php
+++ absen/index.php
@@ -40,12 +40,16 @@
 </head>
 
 <body background="bona123.jpg">
-
-<form action="loginnindya.php" method="post">
-<div class="container" style="height:250"></div>
+<div style="height:30"></div>
 <div class="row">
-  <div class="col-md-2"></div>
-  <div class="col-md-6" style="height:30"><input type="text" name="NIM" style="height:30; width:520;font-size:28px;text-align:center"></div>
+<div class="col-md-1" ></div>
+<div  class="col-md-11" style="width:200; height:200"><img class="img-responsive" src="logoBSLC.png"></div>
+</div>
+<form action="loginnindya.php" method="post">
+<div class="container" style="height:20"></div>
+<div class="row">
+  <div class="col-md-2" style="text-align:right; font-size:30px; font-family:Arial, Helvetica, sans-serif; color:#09F">NIM</div>
+  <div class="col-md-6" style="height:30"><input type="text" name="NIM" style="height:40; width:520;font-size:26px;text-align:center" class="form-control" placeholder="Masukkan NIM Anda"></div>
   <div><input name="input" style="width:300" type="submit" value="Login" class="btn btn-info btn-lg"></div>
 </div>
 
@@ -61,6 +65,7 @@
   ?>
 </div>
 </div>
+<br>
 
 
 <!--<div class="row">  
@@ -70,12 +75,17 @@
 <!--  </div> -->
 
 
+<div class="row">
+  <div class="col-md-7" style="width:875"></div>
+  <div class="col-md-3" style="height:40">
+  <div style="font-size:28px;text-align:center; color:#FFF;background-color:#09F; width:450"><b>PICKET LIST</b></div></div>
+  </div>
+
   <div class="container"></div>
   <div class="row">
-  <div class="col-md-7"></div>
-  <div class="col-md-5" style="height:150;width:50"></div>
-  <div>
-    <nav>
+  <div class="col-md-9" style="width:890"></div>
+  <div class="col-md-3" style="height:200;width:450;background-color:#FFF">
+  <nav>
       <ul style="font-size:18px">
 
         <?php 
@@ -93,6 +103,7 @@
     </nav>
   </div>
   </div>
+
 
   <!-- Note to INDRA : tolong taro tombol admin ini ya, dari <a href= ...> sampai </a>-->
   <!-- Tombol Admin -->
